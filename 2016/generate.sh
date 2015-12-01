ef4eb02acde723a9ace6e34e909d30ca36e5bafa
Matt Hinchliffe <matt.hinchliffe@ft.com>
2015-12-01 18:17:10 +0000

    Setup demos for colour sprites

--- 2016/generate.sh
+++ 2016/generate.sh
@@ -1,6 +1,10 @@
 for entry in svg/*
 do
+  if ! [ -d demos/local/svg ];then
+    mkdir demos/local/svg
+  fi
+
   if [ -f "$entry" ];then
-    node 2016/sprite.js --icon="$entry" --colors=000000,333333,777777,b0b0b0,ffffff,2e6e9e,92288f,9e2f50,cc0000,d66d06 > 2016/output/"$entry"
+    node 2016/sprite.js --icon="$entry" --colors=000000,333333,777777,b0b0b0,ffffff,2e6e9e,92288f,9e2f50,cc0000,d66d06 > demos/local/"$entry"
   fi
 done
